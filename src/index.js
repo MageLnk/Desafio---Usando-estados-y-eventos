ac69cf33fd3b658999a081fca796402e8a2e3fb8
MageLink <mage.link.zhezard@gmail.com>
2022-08-26 20:08:08 -0400

    Starting

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+// Components
+import App from "./App";
+// Style
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
